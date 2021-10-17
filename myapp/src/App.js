6eed19cba5c87bd5d5261bcf51bf61032e465383
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-10-17 10:03:09 +0100

    nesting components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>React App!</h1>
-      <p>Shall We :)</p>
-    </div>
-  );
+import React, { Component } from 'react';
+import Ninjas from "./Ninjas";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App!</h1>
+        <p>Shall We :)</p>
+        <Ninjas />
+      </div>
+    );
+  }    
 }
 
 export default App;
