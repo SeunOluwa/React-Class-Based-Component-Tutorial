51152044295918e73c9fd725c1f539171def1655
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-10-17 14:35:51 +0100

    forms revisited

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from "./Ninjas";
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -15,6 +16,7 @@ class App extends Component {
         <h1>React App!</h1>
         <p>Shall We :)</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }    
